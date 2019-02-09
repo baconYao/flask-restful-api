9af1e56398cd78f86ddcbafef99aa7692df0c020
baconYao <bacon735392@gmail.com>
2019-02-09 16:14:58 +0800

    Use reqparser to check payload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -13,6 +13,13 @@ jtw = JWT(app, authenticate, identity)      # POST /auth
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help='This field cannot be left blank'
+    )
+    
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -22,7 +29,8 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message':'An item with name {} already exists.'.format(name)}, 400
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
@@ -35,7 +43,8 @@ class Item(Resource):
     
     @jwt_required()
     def put(self, name):
-        data = request.get_json()
+        
+        data = Item.parser.parse_args()
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
