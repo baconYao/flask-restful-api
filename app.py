4e6957dfb5654b1a6e677c22706561d4b903903c
baconYao <bacon735392@gmail.com>
2019-02-14 07:56:28 +0800

    Refactor and add User endpoints

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_restful import Api
 from flask_jwt import JWT
 # Ours
 from security import authenticate, identity
-from resources.user import UserRegister
+from resources.user import UserRegister, User
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -22,10 +22,11 @@ from resources.store import Store, StoreList
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'asasdasf'
 api = Api(app)
 
-# 自動建立data.db (定義在22行)
+# 自動建立data.db (定義在23行)
 @app.before_first_request           # 在第一個request進來之前
 def create_tables():
     db.create_all()
@@ -37,6 +38,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
 
 if __name__ == "__main__":
     from db import db
